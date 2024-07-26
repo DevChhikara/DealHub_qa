03bfc0463a05ce37cef1192f6b33b024251d131b
KartikChawla09 <kartik.chawla.ug21@nsut.ac.in>
2024-07-26 14:05:49 +0530

    RiskFactor Complete without Services

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -7,5 +7,13 @@ const UserController = require("../controllers/userController");
 const RiskFactorController = require("../controllers/RiskFactorController");
 router.post("/GetClientKey", UserController.getClientKey);
 router.post("/Login", UserController.verifyLogin);
-router.post("/getRiskFactor", RiskFactorController.sp_get_riskfactor_masters);
+router.post(
+  "/GetRiskFactorMaster",
+  RiskFactorController.sp_get_riskfactor_masters
+);
+router.post("/GetRiskdetails", RiskFactorController.getRiskDetails);
+router.post(
+  "/RiskFactorInsertUpdate",
+  RiskFactorController.insertOrUpdateRiskFactor
+);
 module.exports = router;
